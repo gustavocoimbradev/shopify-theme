51b0ac72e0b6965215fcd208311858a3a06cdc66
Gustavo Coimbra <whoisgustavolima@gmail.com>
2025-04-06 21:51:57 -0300

    telecenter-eletronica

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $(".js-hero").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 3000
+    })
+
+}) 
