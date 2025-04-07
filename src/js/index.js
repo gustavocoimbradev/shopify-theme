9a2c92dff9bb671b1a3425d31d3eca19b393b521
Gustavo Coimbra <whoisgustavolima@gmail.com>
2025-04-07 00:53:07 -0300

    telecenter-eletronica

--- src/js/index.js
+++ src/js/index.js
@@ -10,10 +10,11 @@ $(function(){
         arrows: false,
         dots: false
     })
-    
+
     $(".js-single-collection").slick({
         slidesToShow: 4,
         slidesToScroll: 1,
+        infinite: false,
         prevArrow: `<button type="button" class="slick-prev" style="transform: rotate(180deg); background: none; border: none;">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
             <path fill="currentColor" d="M11.273 3.687a1 1 0 1 1 1.454-1.374l8.5 9a1 1 0 0 1 0 1.374l-8.5 9.001a1 1 0 1 1-1.454-1.373L19.125 12z"/>
