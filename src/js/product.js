e0c2dad17710fb0914db785929cb3a4e6e0b5e7e
Gustavo Lima <whoisgustavolima@gmail.com>
2025-04-16 08:37:55 -0300

    telecenter-eletronica

--- src/js/product.js
+++ src/js/product.js
@@ -1,29 +1,23 @@
 $(function(){
 
-    $(".js-hero").slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        infinite: true,
-        autoplaySpeed: 3000,
-        fade: true,
-        arrows: false,
-        dots: false
-    })
+    var $container = $(".js-products-random");
+    var $items = $container.find(".js-product-card").toArray();
+    var shuffled = $items.sort(function(){ return 0.5 - Math.random(); }).slice(0, 4);
+    $container.empty().append(shuffled);
 
     $(".js-products-single-line").slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         infinite: true,
         prevArrow: `<button type="button" class="slick-prev" style="transform: rotate(180deg); background: none; border: none;">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
             <path fill="currentColor" d="M11.273 3.687a1 1 0 1 1 1.454-1.374l8.5 9a1 1 0 0 1 0 1.374l-8.5 9.001a1 1 0 1 1-1.454-1.373L19.125 12z"/>
-          </svg>
+        </svg>
         </button>`,
         nextArrow: `<button type="button" class="slick-next" style="background: none; border: none;">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
             <path fill="currentColor" d="M11.273 3.687a1 1 0 1 1 1.454-1.374l8.5 9a1 1 0 0 1 0 1.374l-8.5 9.001a1 1 0 1 1-1.454-1.373L19.125 12z"/>
-          </svg>
+        </svg>
         </button>`,
         responsive: [
             {
@@ -36,6 +30,6 @@ $(function(){
                 }
             }
         ]
-      });
+    });
 
-}) 
+})
