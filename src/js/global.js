01197812fc29612c35bf7a608d61a29851d64644
Gustavo Lima <whoisgustavolima@gmail.com>
2025-06-24 10:21:32 -0300

    telecenter-eletronica

--- src/js/global.js
+++ src/js/global.js
@@ -1,25 +1,41 @@
 $(function () {
 
-    // Links
-    $('a').filter(function () {
-      const href = $(this).attr('href');
-      return href === '#' || href === '';
-    }).attr('href', 'javascript:void(0)');
+  // Links
+  $('a').filter(function () {
+    const href = $(this).attr('href');
+    return href === '#' || href === '';
+  }).attr('href', 'javascript:void(0)');
 
-    // Header
-    function checkScroll() {
-      const header = document.querySelector('.js-header');
-      if (!header) return;
-      if (window.scrollY > 20) {
-        header.classList.add('is-active');
-      } else {
-        header.classList.remove('is-active');
-      }
+  // Header
+  function checkScroll() {
+    const header = document.querySelector('.js-header');
+    if (!header) return;
+    if (window.scrollY > 20) {
+      header.classList.add('is-active');
+    } else {
+      header.classList.remove('is-active');
     }
-  
-    window.addEventListener('load', checkScroll);
-    window.addEventListener('resize', checkScroll);
-    window.addEventListener('scroll', checkScroll);
+  }
+
+  $(".js-toggle-search").on("click", function () {
+    $(".js-search").toggleClass("is-active");
+    $("html,body").toggleClass("no-scroll-mobile");
+  })
+
+  setInterval(function () {
+    if ($(".js-header").hasClass("is-active")) {
+      if ($(".js-search").hasClass("is-active")) {
+        $(".js-avoid-search").addClass("is-avoiding");
+      } else {
+        $(".js-avoid-search").removeClass("is-avoiding");
+      }
+    } else {
+      $(".js-avoid-search").removeClass("is-avoiding");
+    }
+  }, 100);
+
+  window.addEventListener('load', checkScroll);
+  window.addEventListener('resize', checkScroll);
+  window.addEventListener('scroll', checkScroll);
 
 });
-   
